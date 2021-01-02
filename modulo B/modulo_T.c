4348f8aded97db217c9c6bfdbdf7c1cf9b991f93
Laura-Rodrigues <62083148+Laura-Rodrigues@users.noreply.github.com>
2021-01-02 00:19:45 +0000

    Módulo T completamente funcional + texto da consola

--- modulo B/modulo_T.c
+++ modulo B/modulo_T.c
@@ -11,6 +11,8 @@ void modulo_T (char *ficheiroFreq, char *ficheiroCod){
         exit (1);
     }
     else{
+        printf("Módulo: t (cálculo dos códigos dos símbolos)\n"); // texto da consola
+
         int b=1;
         FILE *cod=fopen(ficheiroCod, "wb");
         
@@ -18,13 +20,17 @@ void modulo_T (char *ficheiroFreq, char *ficheiroCod){
         char rn;
         fscanf(f,"@%c@%lu@",&rn,&blocos);
         fprintf(cod,"@%c@%lu",rn,blocos); //imprime no .cod a parte inicial (R/N e o número de blocos)
+        printf("Número de blocos: %lu\n", blocos); // texto da consola
+        printf("Tamanho dos blocos analisados no ficheiro de símbolos: "); // texto da consola
 
+        
         while(b <= blocos){ // contador b permite executar este código para o número de blocos certo
 
             unsigned long tamBloco;
             fscanf(f,"%lu@",&tamBloco);
             fprintf (cod, "@%lu@",tamBloco); //imprime no .cod o tamanho do bloco que está a ser lido
-
+            printf("%lu",tamBloco); // texto da consola
+            
             int *freqs = malloc(256 * sizeof(int)), *simbolos = malloc(256 * sizeof(int));
             freqToArray(freqs,f);
             decrescenteSortSimb(freqs,simbolos,256);
@@ -33,7 +39,7 @@ void modulo_T (char *ficheiroFreq, char *ficheiroCod){
             unsigned char *codes[256];
             for(int i=0;i<256;i++) {
                 codes[i]=(unsigned char *)malloc(sizeof(unsigned char *)*256);
-                //*(codes[i])=NULL;
+                *(codes[i])=NULL;
             }
 
             shannon(freqs,codes,0,255);
@@ -45,8 +51,11 @@ void modulo_T (char *ficheiroFreq, char *ficheiroCod){
 
             free(simbolos);
             free(freqs);
+            
+            if(b<blocos) printf("/");  // texto da consola
             b++;
         }
+        printf(" bytes");  // texto da consola
         fprintf(cod,"@0"); 
         fclose(f);
         fclose(cod);
@@ -54,45 +63,36 @@ void modulo_T (char *ficheiroFreq, char *ficheiroCod){
 }
 
 int main() {
-    char freq[] = {"aaa.txt.freq"};
-    char freq2[] = {"aaa.txt.rle.freq"};
-
-
-    char filename[30], extension[8], txt[3],aux[30];
-    strcpy(aux,freq);
-    sscanf(freq,"%[^.].%s",filename,extension); // guarda o nome do ficheiro dado em filename
-    sscanf(extension,"%[^.].%s",txt,extension); // guarda a extensão que está após .txt. para depois avaliar se é um .freq ou um .rle.freq
-
+    char *freq_name="aaa.txt.freq", *cod_name, *freq_extension=".freq",
+            *cod_extension=".cod", *extension;
 
     //escrita da extensão correta no ficheiro cod que vai ser criado
-    if(strcmp (extension,"freq") == 0) {
-        strcat(filename,".txt.cod(2)");
-    }
-    else if(strcmp (extension,"rle.freq") == 0) {
-        strcat(filename,".txt.rle.cod(2)");
-    }
-    else printf("Wrong extension - ");
+    cod_name=(char *)malloc(strlen(freq_name)+1);
+
+    extension=freq_name+strlen(freq_name)-strlen(freq_extension);
+
+    if (strcmp(extension,freq_extension)) printf("\n Wrong extension! \n");
+
+    else{
+        strcpy(cod_name, freq_name);
+        extension=cod_name+strlen(cod_name)-strlen(freq_extension);
+        strcpy(extension,cod_extension);
+
+        clock_t begin = clock();
+        modulo_T(freq_name,cod_name);
+        clock_t end = clock();
+        double time_spent = (double)(end - begin) / CLOCKS_PER_SEC; // para escrever na consola o tempo que demorou a executar o módulo
 
 
-    clock_t begin = clock();
-    modulo_T(aux,filename);
 
+    //time_t now = time ( NULL );
+    //struct tm *date = localtime ( &now ); // para escrever na consola a data correta
 
-    
-    clock_t end = clock();
-    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC; // para escrever na consola o tempo que demorou a executar o módulo
+    //printf("\n\nCláudia Silva, a93177, Laura Rodrigues, a93169, MIEI/CD, %d-%d-%d\n",date->tm_mday,date->tm_mon + 1,date->tm_year + 1900);
 
-    time_t now = time ( NULL );
-    struct tm *date = localtime ( &now ); // para escrever na consola a data correta
-    
     // Texto de saída na consola
-    printf("Cláudia Silva, a93177, Laura Rodrigues, a93169, MIEI/CD, %d-%d-%d\n",date->tm_mday,date->tm_mon + 1,date->tm_year + 1900);
-    printf("Módulo: t (cálculo dos códigos dos símbolos)\n");
-    // printf ("Número de blocos: %d",c);
-    printf("Tamanho dos blocos analisados no ficheiro de símbolos: bytes\n");
-    printf("Tempo de execução do módulo (milissegundos): %f ms\n", time_spent*1000);
-    printf("Ficheiro gerado: %s", filename);
-
-     
+    printf("\nTempo de execução do módulo (milissegundos): %f ms\n", time_spent*1000);
+    printf("Ficheiro gerado: %s", cod_name);
+}  
     return 0;
 }
