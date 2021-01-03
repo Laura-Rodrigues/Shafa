cb1f71498da6e6d6666857a0ac0d9b1720952925
MarianaFilipa <62109899+MarianaFilipa@users.noreply.github.com>
2021-01-03 15:46:11 +0000

    Update ModuloF.c

--- Módulo F/ModuloF.c
+++ Módulo F/ModuloF.c
@@ -1,21 +1,16 @@
-/* Fase 1:
 
-2) Se com a compressão o ultimo bloco <1K????
-NOTA: APRESENTAR INFORMAÇOES PARA CADA MODULO
+/*
+    Módulo F - parte do trabalho shafa grupo 16, UC de Comunicação de Dados MIEI 2020-2021
+    Período de desenvolvimento de trabalho: 2 de dezembro de 2020 a 3 de janeiro de 2021
+    Autores (por ordel alfabética):
+    Francisco Toldy   a93226
+    Mariana Rodrigues a93306;
 
-NA MAIN DO TRABALHO:
--> se pedir para fazer -m f -m c, fazer modulo f e dizer que não dá para fazer o modulo c porque falta o .cod;
-
-METODO 1: *****Passar ler um bloco e escrever logo a seguir (aka processo atual)*****
-    1) Ler um bloco do ficheiro;
-    2) Determinar se vale a pena fazer o RLE;
-    2.1) Aplicar o metodo RLE (e gravar file .rle)
-    3) Calcular as frequências dos simbolos 
-    4) Escrever no novo file as frequências
-    5) Repetir este processo para todos os blocos
-
-Nota: usamos unsigned char porque cada char corresponde a 1 byte aka 8 bits
+    Este módulo inclui funções como "toRLE": compressão RLE; "gravarFrequências": grava as frequências no file .freq; "calculaFreqs": calcula a frequẽncia de cada byte; "inicializaFile": criação dos files que serão output do programa;
+ "funcaoFREQ": função auxiliar para obter os ficheiros .freq; "printTerminal": escrita das informações no terminal; "moduloF": principal função do modulo.
 */
+
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,18 +19,13 @@ Nota: usamos unsigned char porque cada char corresponde a 1 byte aka 8 bits
 #include "string.h"
 #include "fsize.h"
 
-/*
-    Esta função tem como objetivo calcular a taxa de compressão. Recebe o numero de bytes do bloco original e o 
-    numero de bytes do bloco pós compressão RLE
-*/
+
 float compressao (unsigned long antes, unsigned long depois){
     float c = (((antes*1.0) - (depois*1.0)) / (antes*1.0));
     return c;
 }
 
-/*
-    Função que avalia se a compressão RLE compensa, retorna o valor 1 caso a taxa de compressão seja mais que 0.05 ou 5% (valor definido pelos docentes)
-*/
+
 float fazerRLE ( unsigned long bytes_lidos, unsigned long tamArrRLE){
     int result = 0;
     float aux = compressao(bytes_lidos, tamArrRLE);
@@ -43,32 +33,24 @@ float fazerRLE ( unsigned long bytes_lidos, unsigned long tamArrRLE){
     return result;
 }
 
-/*
-    Função tem como propósito fazer a compressão RLE de um bloco de bytes de tamanho tamBlock e guardar o bloco covertido no array arr.
-    A função analisa o bloco byte a byte, incrementando o numero de repetições a cada vez que o byte atual é igual ao seguinte.
-    Quando o byte atual for diferente e igual a zero ou diferente e o numero de repetições (r) for maior que 3, o arr aumenta em 3 bytes. O primeiro byte
-    guarda o byte 0, o segundo o byte que se está a repetir, e o terceiro guarda o nr de repetições.
-    Na situação restante (quando o byte atual é diferente do seguinte mas o numero de repetições nao é superior a 3), o ciclo for aloca memória necessaria para
-    colocar r repetições do byte atual
-*/
+
 unsigned long toRLE(unsigned char bloco[],unsigned long tamBlock,unsigned char *arr){ 
     int r = 1;
     unsigned long j = 0;
-    for ( int i = 0; i<tamBlock ;i++){
+    for ( unsigned long i = 0; i<tamBlock ;i++){
         if (bloco[i] == bloco[i+1] && r != 255) r++;
-        else if  (r == 255 || bloco[i] == 0 || r > 3)  { //nao esquecer da situaçao que o r é smp < 255
-            arr = (unsigned char *)realloc(arr,j * sizeof (unsigned char) + 3* sizeof (unsigned char));
+        else if  (r == 255 || bloco[i] == 0 || r > 3)  { 
             arr[j]=0;
             arr[j+1] = bloco[i];
-            arr[j+2] = r;
+            arr[j+2]  = r;
             r = 1;
             j+=3;
         }
         else{
-            for (r; r>=1;r--){
-                arr = (unsigned char *)realloc(arr, j * sizeof (unsigned char) + sizeof (unsigned char));
+            while (r>=1){
                 arr[j] = bloco[i];
                 j++;
+                r--;
             }
             r=1;
         }       
@@ -76,25 +58,7 @@ unsigned long toRLE(unsigned char bloco[],unsigned long tamBlock,unsigned char *
     return j;
 }
 
-/*
-    Função que recebe um bloco, o numero de bytes lidos e um int que sinaliza se é o primeiro bloco ou um dos blocos seguintes
-    Se se tratar do 1º bloco (rep == 0) então a função fopen será chamada com "wb", caso não seja, a função fopen é chamada com "ab", 
-    ambas as situações escrevem em binário
-*/
-void gravarRLE (char *nomeficheiro, unsigned char *bloco, unsigned long tamRLE, int rep){
-    FILE *fp;
-    if (rep == 0) fp = fopen (nomeficheiro, "wb");
-    else fp = fopen (nomeficheiro, "ab");
-    fwrite(bloco,1,tamRLE,fp);
-    fclose(fp);
-}
-
-/*
-    Função que recebe um array de frequencias de um bloco, o tamanho do mesmo bloco e, com um ciclo for, grava no ficheiro as frequencias desse bloco
-    e o tamanho do bloco na notação apropriada
-*/
-void gravarFrequencias (char *nomeficheiro, unsigned long tamBloco, unsigned long *freq){
-    FILE *fp = fopen(nomeficheiro, "a");
+void gravarFrequencias (FILE *fp, unsigned long tamBloco, unsigned long *freq){
     if (fp != NULL){
         fprintf ( fp, "@%lu@",tamBloco);
         for (int i = 0; i < 256; i++) {
@@ -104,54 +68,15 @@ void gravarFrequencias (char *nomeficheiro, unsigned long tamBloco, unsigned lon
             else fprintf(fp, "%lu;", freq[i]);
         }
     }
-    fclose(fp);
 }
 
-/*
-    Função que acrescenta um apendice inteiro a um ficheiro
-*/
-void acrescentaNumFile (char *nomeficheiro, int n){
-    FILE *fp = fopen(nomeficheiro, "a");
-    if (fp != NULL)
-        fprintf(fp, "@%d",n);
-    fclose (fp);
-}
-
-/*
-    Função que calcula a frequência de cada caracter
-*/
 void calculaFreqs (unsigned char *bufferBloco, unsigned long *freq, unsigned long tamBloco){
-    int i;
+    unsigned long i;
     for (i = 0; i < tamBloco; i++){
         freq[bufferBloco[i]]++;
     }
 }
 
-/*
-    Função que lê um ficheiro e passa os dados guardados nos bytes correspondentes ao tamanho de um bloco e passa para um buffer bloco desse mesmo tamanho
-*/
-unsigned long fileToBloco (char *fileInicial, unsigned long tamBloco, unsigned long posicao, unsigned char *bufferBloco){
-    FILE *fp = fopen(fileInicial, "rb");
-    unsigned long  bytes_lidos;
-
-    if (fp == NULL ){
-        printf("erro relacionado com o file\n");
-        return 0;
-    }
-    fseek (fp, posicao, SEEK_SET);
-    bytes_lidos = fread (bufferBloco,sizeof(unsigned char), tamBloco,fp); 
-
-    fclose(fp);
-    return bytes_lidos;
-}
-
-/*
-    Função que define o sufixo do nome do ficheiro gerado e tambem o prefixo @R ou @N dentro do ficheiro, consoante a opção escolhida indicada pelo int
-    modulo.
-    modulo = 0 -> ficheiro .RLE
-    modulo = 1 -> ficherio .RLE.FREQ
-    modulo = 2 -> ficheiro .FREQ
-*/
 void inicializaFile (char *nomefile, int modulo, char *nomeFinal , long long nblock){
     strcpy(nomeFinal, nomefile); 
     if (modulo == 0) strcat(nomeFinal, ".rle");
@@ -164,13 +89,13 @@ void inicializaFile (char *nomefile, int modulo, char *nomeFinal , long long nbl
         }
         if (modulo == 2){
             fprintf(fp, "@N");
-            }
+        }
+        if ( modulo == 1 || modulo == 2 ) fprintf(fp,"@%lld",nblock);
         fclose (fp);
     }    
-    if ( modulo == 1 || modulo == 2 ) acrescentaNumFile(nomeFinal, nblock);
 }
 
-unsigned long long  tamfileAux (char *file, unsigned long *tamBlock){
+unsigned long long  tamfile (char *file, unsigned long *tamBlock){
     long size_of_last_block;
     FILE *fp = fopen (file, "rb");
     long long nblock = fsize (fp, NULL, tamBlock ,&size_of_last_block);
@@ -179,99 +104,99 @@ unsigned long long  tamfileAux (char *file, unsigned long *tamBlock){
     return sizefile;
 }
 
-int moduloF (char *fileInicial, unsigned long tamBlock, int obrigatorioRLE){
-    clock_t begin = clock();
+
+void funcaoFREQ (FILE *fileF, unsigned long tamanho, unsigned char *bloco){
+    unsigned long freq[256];
+    for (int i = 0; i < 256; i++) freq[i] = 0;
+    calculaFreqs(bloco,freq, tamanho);
+    gravarFrequencias(fileF, tamanho, freq );
+}
+
+
+void printfTerminal (unsigned long *arrayTamRLE, long long nblock, unsigned long tamBlock, unsigned long size_of_last_block, char *fileFREQ, char *fileRLE, int obrigatorio,  double time_spent, unsigned long long sizefile ){
     time_t now = time ( NULL );
-    struct tm *date = localtime ( &now );
+    struct tm *date = localtime (&now);
 
-    char fileFREQ[100], fileRLE[100];
-    long size_of_last_block;
-    unsigned long bytes_lidos, freq[256], tamRLE;
-    FILE *fp = fopen (fileInicial, "rb");
-    long long nblock = fsize (fp, NULL, &tamBlock ,&size_of_last_block);
-    fclose(fp);
-    unsigned long arrayTamRLE[nblock];
-    unsigned long long sizefile = (nblock-1) * tamBlock + size_of_last_block, sizefileRLE;
-    unsigned char *auxRLE,*bufferBloco;  
-
-    bufferBloco = (unsigned char*)malloc(tamBlock * sizeof(unsigned char));
-    bytes_lidos = fileToBloco(fileInicial, tamBlock, 0, bufferBloco);
-    auxRLE = (unsigned char *)malloc(sizeof(unsigned char));
-    tamRLE = toRLE (bufferBloco, bytes_lidos, auxRLE);
-
-    if (nblock > 1 && size_of_last_block < 1024 ){
-        nblock --;
-        size_of_last_block += tamBlock;
-    }
-
-    if (obrigatorioRLE == 1 || fazerRLE(bytes_lidos,tamRLE)){ 
-        inicializaFile(fileInicial, 0, fileRLE, nblock);
-        inicializaFile(fileInicial, 1, fileFREQ, nblock);
-      
-        for ( int i = 0; i < nblock; i++){
-            if ( i == nblock - 1 ){
-                bufferBloco = (unsigned char *)malloc(size_of_last_block * sizeof (unsigned char));
-                bytes_lidos = fileToBloco(fileInicial, size_of_last_block, i*tamBlock, bufferBloco);
-            }
-            else{
-                bufferBloco = (unsigned char *)malloc(tamBlock*sizeof (unsigned char));
-                bytes_lidos = fileToBloco(fileInicial, tamBlock, i*tamBlock, bufferBloco);         
-            }    
-            auxRLE = (unsigned char *)malloc(sizeof(unsigned char));
-            tamRLE = toRLE (bufferBloco, bytes_lidos, auxRLE);
-            gravarRLE(fileRLE, auxRLE, tamRLE, i);
-        
-            for (int i = 0; i < 256; i++) freq[i] = 0;
-            calculaFreqs(auxRLE,freq,tamRLE);
-            gravarFrequencias(fileFREQ, tamRLE, freq );
-            arrayTamRLE[i] = tamRLE;
-        }
-
-        acrescentaNumFile(fileFREQ, 0);
-        obrigatorioRLE = 1;
-
-        sizefileRLE = tamfileAux (fileRLE, &tamBlock);
-    }
-    else{
-        inicializaFile(fileInicial, 2, fileFREQ, nblock);
-        for ( int j = 0; j < nblock; j++ ){
-            for (int i = 0; i < 256; i++) freq[i] = 0;
-            if (j == nblock - 1){
-                bufferBloco = malloc(size_of_last_block*sizeof (unsigned char));
-                bytes_lidos = fileToBloco(fileInicial, size_of_last_block, j*tamBlock, bufferBloco);
-            }
-            else{
-                bufferBloco = malloc(tamBlock * sizeof (unsigned char));
-                bytes_lidos = fileToBloco(fileInicial, tamBlock, j*tamBlock, bufferBloco);
-            }
-            calculaFreqs(bufferBloco,freq,bytes_lidos);
-            gravarFrequencias(fileFREQ, bytes_lidos, freq );
-            free(bufferBloco);
-        }
-        acrescentaNumFile(fileFREQ, 0);
-    }
-    clock_t end = clock();
-    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
- 
-    printf("Francisco José Martinho Toldy,a93226; Mariana Filipa Rodrigues,a93306 MIEI-CD,%d-%d-%d\n",date->tm_mday,date->tm_mon + 1,date->tm_year + 1900);
-    printf("Módulo: f \nNúmero de blocos: %lld\n", nblock);//,numBlocos
+    printf("Francisco Toldy,a93226; Mariana Rodrigues,a93306 MIEI-CD,%d-%d-%d\n",date->tm_mday,date->tm_mon + 1,date->tm_year + 1900);
+    printf("Módulo: f \nNúmero de blocos: %lld\n", nblock);
     printf("Tamanho dos blocos analisados: %lld blocos de %lu bytes + 1 bloco de %lu bytes\n", nblock-1, tamBlock, size_of_last_block); 
-    if (obrigatorioRLE) {
-        printf("Compressão RLE: %s (%0.2f %% compressão)\n",fileRLE, compressao(sizefile,sizefileRLE)*100);//S ou N,tamanho e taxa de compressão
+   
+    if ( obrigatorio == 1){
+        unsigned long long sizefileRLE = tamfile (fileRLE, &tamBlock);
+        float compFile = compressao(sizefile,sizefileRLE);
+        printf("Compressão RLE: %s (%0.2f %% compressão)\n",fileRLE, compFile*100);
         printf("Tamanho dos blocos analisados no ficheiro RLE:");
         for ( int i = 0; i < nblock; i++){
-            if (i == nblock-1) printf("%lu\n", arrayTamRLE[i]);
-            else printf("%lu/",arrayTamRLE [i]);
+        if (i == nblock-1) printf("%lu\n", arrayTamRLE[i]);
+        else printf("%lu/",arrayTamRLE [i]);
         }
     }
     printf("Tempo de execução do módulo:%0.2f ms\n", time_spent*1000);//função que calcula o tempo de execução
-    printf("Ficheiros gerados: %s \n", fileFREQ);// lista de ficheiros gerados
+    printf("Ficheiros gerados: %s \n", fileFREQ);
+}
 
+
+int moduloF (char *fileInicial, unsigned long tamBlock, int *obrigatorioRLE){
+    clock_t begin = clock();
+    char fileFREQ[100], fileRLE[100];
+    long size_of_last_block;
+    unsigned long bytes_lidos, tamRLE;
+    FILE *fileI = fopen (fileInicial, "rb");
+    long long nblock = fsize (fileI, NULL, &tamBlock ,&size_of_last_block);    // cálculo do numero de blocos
+    unsigned long long sizefile = (nblock-1) * tamBlock + size_of_last_block;  // tamanho do ficheiro
+    unsigned char *auxRLE,*bufferBloco;  
+    if (nblock > 1 && size_of_last_block < 1024){                              // condição para quando o ultimo bloco é menor que 1 Kb
+        nblock--;
+        size_of_last_block += tamBlock;
+    }
+    unsigned long arrayTamRLE[nblock];                                         // o array que regista o tamanho dos blocos da compressão RLE, para ser posteriormente apresentado no terminal
+    if ( size_of_last_block > tamBlock ) bufferBloco = (unsigned char*)malloc(size_of_last_block * sizeof(unsigned char));
+    else bufferBloco = (unsigned char*)malloc(tamBlock * sizeof(unsigned char));
+    if (nblock == 1) bytes_lidos = fread (bufferBloco,sizeof(unsigned char), sizefile,fileI); 
+    else bytes_lidos = fread (bufferBloco,sizeof(unsigned char), tamBlock,fileI);                  // leitura do bloco para o buffer
+    auxRLE = (unsigned char *)malloc(bytes_lidos*3*sizeof(unsigned char));                         // alocação de memória para a compressão RLE
+    tamRLE = toRLE (bufferBloco, bytes_lidos, auxRLE);                                             // compressão RLE é feita e o tamanho do bloco resultante é guardado em tamRLE
+    if (*obrigatorioRLE == 1 || fazerRLE(bytes_lidos,tamRLE)){ 
+        inicializaFile(fileInicial, 0, fileRLE, nblock);                                           //Criação dos novos files
+        FILE *fileR = fopen (fileRLE, "wb");
+        inicializaFile(fileInicial, 1, fileFREQ, nblock);
+        FILE *fileF = fopen (fileFREQ, "a");
+        fwrite(auxRLE,1,tamRLE,fileR);                                                             // primeiro bloco é guardado no ficheiro
+        arrayTamRLE[0] = tamRLE;                                                                   // tamanho do primeiro bloco é anotado no array
+        funcaoFREQ(fileF, tamRLE, auxRLE);
+        for ( int i = 1; i < nblock; i++){                                                         // ciclo para o resto dos blocos, segue a seguinte estrutura:
+            if ( size_of_last_block > tamBlock && i == nblock - 1) bytes_lidos = fread (bufferBloco,sizeof(unsigned char), size_of_last_block,fileI); // leitura de um novo bloco do ficheiro original
+            else bytes_lidos = fread (bufferBloco,sizeof(unsigned char), tamBlock,fileI);          
+            tamRLE = toRLE (bufferBloco, bytes_lidos, auxRLE);                                     // compreessão RLE desse bloco
+            fwrite(auxRLE,1,tamRLE,fileR);                                                         // escrita desse bloco no novo ficheiro
+            funcaoFREQ(fileF, tamRLE, auxRLE);                                                     // calculo da frequência de cada bytes e escrita num ficheiro
+            arrayTamRLE[i] = tamRLE;                                                               // tamanho do é anotado no array
+        }
+        fprintf(fileF,"@%d",0);                                                                   // 0 no fim do ficheiro freq
+        fclose(fileF);
+        fclose(fileR);
+        *obrigatorioRLE = 1;           
+    }
+    else{                                                                                         // cenário em que a compressão RLE não é efetuada
+        inicializaFile(fileInicial, 2, fileFREQ, nblock);                                         // criação do file .freq
+        FILE *fileF = fopen (fileFREQ, "a");
+        funcaoFREQ(fileF, bytes_lidos, bufferBloco);                                              // função auxiliar que calcula e grava as frequências
+        for ( int j = 1; j < nblock; j++ ){                                                       // repetição do processo para os restantes blocos:
+            if ( size_of_last_block > tamBlock && j == nblock - 1) bytes_lidos = fread (bufferBloco,sizeof(unsigned char), size_of_last_block,fileI);  //leitura de um novo bloco do ficheiro original
+            else bytes_lidos = fread (bufferBloco,sizeof(unsigned char), tamBlock,fileI);
+            funcaoFREQ(fileF, bytes_lidos, bufferBloco);                                          // calculo da frequência de cada bytes e escrita num ficheiro
+        }
+        fprintf(fileF,"@%d",0);                                                                   // 0 no fim do ficheiro freq
+        
+        fclose(fileF);        
+    }
+    free(auxRLE);
+    free(bufferBloco);
+    fclose(fileI);
+    
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;                                   
+    printfTerminal(arrayTamRLE, nblock, tamBlock,size_of_last_block, fileFREQ, fileRLE, *obrigatorioRLE,time_spent, sizefile );       // bloco de texto para o terminal
 
     return 0;
 }
-
-/*  printf("--%lu--\n", tamRLE);
-    for ( int x = 0; x < tamRLE; x++) printf("%u,", auxRLE[x]);
-    printf ("\n");
-*/
