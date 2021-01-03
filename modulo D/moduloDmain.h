75921f2cf53060633c4fd8d44697c85bdbbf0c4b
andrepinto42 <61984266+andrepinto42@users.noreply.github.com>
2021-01-03 16:33:56 +0000

    Update moduloDmain.h

--- modulo D/moduloDmain.h
+++ modulo D/moduloDmain.h
@@ -20,17 +20,17 @@ void bubbleSort(unsigned int arr_cod[],int arr_altura[],int arr_carater[], int n
 int get_num_blocos(FILE *f);
 int get_tam_bloco (FILE *f);
 
-void escreve_ficheiro(char simbolos[],char *filename,int *posicao,int *tamanho);
+void escreve_ficheiro(char simbolos[],char *filename,int *tamanho);
 
 char  *descompressao_shafa(unsigned char arr[],int tam_bloco,NODO *codes,int *tamanho);
 char  *descompressao_shafa_rle(unsigned char arr[],int tam_bloco,NODO *codes,int *tamanho);
-
-int get_info_bloco(FILE *f,int tam_bloco,NODO* nodo,int flag_rle,int descodificar,char filename[]);
+char *get_info_block(FILE *f,int tam_bloco);
+int get_info_bloco(char *array_simbolos,char filename[],int *tamanho);
 unsigned int arr_to_unsigned(char *codi,int N);
 
 NODO *cria_NODOS(unsigned int arr_codigos[],int arr_carater[],int arr_alturas[],int tam);
-NODO* make_NODO(FILE *f,int tam_bloco);
+NODO* make_organizado_NODO(FILE *f,int tam_bloco);
 
-void Dmain(char nome_shaf[],char nome_cod[],int descodificar,char filename[]);
+void Dmain(char nome_shaf[],char nome_cod[],int descodificar_rle,int descodificar_sf,char filename[]);
 
 #endif
