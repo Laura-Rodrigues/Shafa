a5b94cc6f8d3a6bdb30defdca778bae3b95129ad
MarianaFilipa <62109899+MarianaFilipa@users.noreply.github.com>
2021-01-03 20:01:55 +0000

    Update moduloDmain.c

--- moduloDmain.c
+++ moduloDmain.c
@@ -590,6 +590,44 @@ void Dmain2(char nome_shaf[],char nome_cod[],int descodificar_rle,int descodific
 }
 
 
+int Dmain1(int argc, char *shafa, int descodificar_rle,int descodificar_sf)
+{
+    int i;
+    char c;
+    int flag_rle=0;
+
+    if (!shafa){
+        printf("Erro no ficheiro\n");
+        return 1;
+    }
+    if (i = strlen(shafa) <= 9){
+        printf("Nome invalido\n");
+        return 1;
+    }
+    i = strlen(shafa)  - 5; // é removido o ".shaf"
+    char flag_rle_c[4] = {shafa[i-4],shafa[i-3],shafa[i-2],shafa[i-1]};
+ 
+    if (  !strcmp( flag_rle_c,".rle") ){
+        flag_rle = 1;
+        i = i-4;
+    }
+    int k;
+    char cod[i+3];
+    for ( k = 0; k < i; k++){
+        cod[k] = shafa[k];
+    }
+    cod[k] = '\0';
+
+    char filename[k];  
+    strcpy(filename,cod);
+
+    if (flag_rle) strcat(cod,".rle.cod");
+    else strcat(cod,".cod");
+ 
+    Dmain(shafa,cod,descodificar_rle,descodificar_sf,filename);
+
+    return 0;
+}/*
 int Dmain1(int argc, char *argv[])
 {
  int i;
@@ -665,7 +703,7 @@ if ( !strcmp(argv[2],"-m") && !strcmp(argv[3],"d") )
  else printf("Erro no input\n"); 
  return 0;
 
-}
+}*/
 
 
 
